758c2fb529fe4854d0f0dff91bc00334b0a097a8
Vitor <vitorbizarracontato@gmail.com>
2022-12-12 14:30:43 -0300

    first routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sobre-nos', function () {
+    return "Sobre nós";
+});
+
+Route::get('/contato', function () {
+    return "Contato";
+});
