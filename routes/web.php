6374df95a5d7262691494b7cbc1f025822531c45
Vitor <vitorbizarracontato@gmail.com>
2022-12-12 17:27:58 -0300

    Route names, redirect routes and fallback routes

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,39 @@ use Illuminate\Support\Facades\Route;
 //     return "Contato";
 // });
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
 
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function () {
+    return 'Login';
+})->name('site.login');
+
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    })->name('app.clientes');
+
+    Route::get('/fornecedores', function () {
+        return 'Fornecedores';
+    })->name('app.fornecedores');
+
+    Route::get('/produtos', function () {
+        return 'Produtos';
+    })->name('app.produtos');
+});
+
+Route::get('/rota1', function () {
+    return "Rota 1";
+})->name('site.rota1');
+
+Route::get('/rota2', function () {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+// Route::redirect('/rota2', '/rota1');
+
+Route::fallback(function () {
+    return 'A rota acessada não existe. <a href="' . route('site.index') . '">Clique aqui</a> para ir para a página inicial';
+});
